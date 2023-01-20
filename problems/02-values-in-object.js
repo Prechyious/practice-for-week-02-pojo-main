41c4566705082da49e6d8d82a0502c908a877879
Prechyious <lemoprechy@gmail.com>
2023-01-20 20:27:17 +0100

    finished pojo-project

---
+++ problems/02-values-in-object.js
@@ -0,0 +1,34 @@
+/***********************************************************************
+Write a function called `valuesInObject(obj)` that takes in an object and returns
+an array of all the values within that Object.
+
+
+Do this once using using a `for...in` loop and once using `Object.values`.
+
+
+Examples:
+
+let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
+let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
+valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
+valuesInObject(foods); // => ["tart", "sour", "sweet"]
+***********************************************************************/
+
+function valuesInObject(obj) {
+  // Your code here
+  let newArr = [];
+  for (let values in obj) {
+    let val = obj[values];
+    newArr.push(val);
+  }
+  return newArr;
+}
+
+// Using Object.values
+function valuesInObject1(obj) {
+  let arr = Object.values(obj);
+  return arr;
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = valuesInObject;
